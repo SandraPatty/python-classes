808d40aea159a1b5f3931111f3bf9de53a696f2c
SandraPatty <alpatova.ad@phystech.edu>
2023-11-07 11:39:12 +0300

    init

---
+++ 1-matplotlib/1-1/laba1.py
@@ -0,0 +1,42 @@
+import array
+import matplotlib.pyplot as plt
+import os
+import matplotlib.colors as mcolors
+import numpy as np
+
+bc = mcolors.BASE_COLORS
+
+m = 0
+directory = "dead_moroz"
+files = []
+files += os.listdir(directory)
+
+for i in files:
+    X = []
+    Y = []
+    N = "a"
+    s = "dead_moroz" + "/" + str(i)
+    with open(s) as f:
+        lines = f.readlines()
+        N = int(lines[0])
+        for j in range(1, N + 1):
+            x, y = map(float, lines[j].split(" "))
+            X.append(x)
+            Y.append(y)
+
+    num_set = np.random.randint(1, len(mcolors.BASE_COLORS), N)
+    colors = [list(bc.keys())[i] for i in num_set]
+    
+    
+    plt.scatter(X, Y, alpha=0.5, c="r", s=40)
+    plt.scatter(X, Y, alpha=0.4, c=colors, linewidths=2, edgecolors="face", s = 7)
+    plt.xlabel('x label')
+    plt.ylabel('y label')
+    plt.title("Number of points " + str(N))
+
+    plt.legend()
+
+    m+=1
+    plt.gca().set_aspect('equal')
+    plt.savefig("pic" + str(m) +".jpg")
+    plt.show()
---
+++ 1-matplotlib/1-3/laba3.py
@@ -0,0 +1,95 @@
+import array
+import matplotlib.pyplot as plt
+
+font_color = 'sienna'
+colors=['#FFEBCD', '#FFDEAD', '#F4A460', '#FFA07A', '#FF7F50', 
+'#FF6347', '#CD5C5C', '#DC143C']
+colorss = ['coral', '#f7ecb0', '#ffb3e6', '#99ff99', '#66b3ff', '#c7b3fb','#ff6666', '#f9c3b7']
+with open("students.txt") as f:
+    
+    lines = [ i.replace('\n', '').split(";") for i in f.readlines()]
+    preps = sorted(set(lines[i][0] for i in range(len(lines))))
+    groups = sorted(set(lines[i][1] for i in range(len(lines))))
+    marks = sorted(set(int(lines[i][2]) for i in range(len(lines))))
+    
+    
+    fig, axes = plt.subplots(3, 3, figsize=(7, 10), facecolor='#e8f4f0')
+    fig.delaxes(ax= axes[2,2])
+    fig.delaxes(ax= axes[1,2])
+    M = [0]*len(preps)
+    for i in range(len(preps)):
+        s = 0
+        M[i] = [0]*(max(marks)+1)
+        labels = [" "]*(max(marks)+1)
+        for k in range(len(lines)):
+            if lines[k][0] == preps[i]: 
+                a = int(lines[k][2])
+                s+=1
+                M[i][a] += 1
+                labels[a] = M[i][a]
+    
+        ax = axes[i%3, i//3]
+        ax.pie(M[i], labels=labels, labeldistance=.78, radius=1.2, startangle=30, 
+           wedgeprops=dict(width=.5), # For donuts
+           colors=colors, 
+           textprops={'color':'rosybrown', 'fontweight':'bold'})
+            
+        ax.set_title(preps[i], fontsize=10, color=font_color, fontweight='bold')
+        
+        legend = plt.legend(marks, 
+                                prop = { "size": 18 },
+                                bbox_to_anchor=(1.5, 2.2), # Legend position
+                                loc='upper left',  
+                                ncol=1, 
+                                fancybox=True)
+        for text in legend.get_texts():
+            plt.setp(text, color=font_color, alpha=0.7, fontweight='bold')
+            
+    fig.subplots_adjust(wspace=.2) # Space between charts
+
+    title = fig.suptitle('Grades in dependence of teachers', y=.95, fontweight='bold', fontsize=20, color=font_color)
+    # To prevent the title from being cropped
+    plt.subplots_adjust(top=0.85, bottom=0.15)
+    plt.savefig("preps" +".png")
+    plt.show()
+    
+    fig, axes = plt.subplots(2, 3, figsize=(8, 8), facecolor='#e8f4f0')
+ 
+    M = [0]*len(groups)
+    for i in range(len(groups)):
+        s = 0
+        M[i] = [0]*(max(marks)+1)
+        labels = [" "]*(max(marks)+1)
+        for k in range(len(lines)):
+            if lines[k][1] == groups[i]: 
+                a = int(lines[k][2])
+                s+=1
+                M[i][a] += 1
+                labels[a] = M[i][a]
+    
+        ax = axes[i%2, i//2]
+        ax.pie(M[i], labels=labels, labeldistance=.78, radius=1.1, startangle=30, 
+           wedgeprops=dict(width=.7), # For donuts
+           colors=colors, 
+           textprops={'color':'rosybrown', 'fontweight':'bold', "size": 11, 'alpha':0.8})
+            
+        ax.set_title(groups[i], fontsize=15, y=0.95, color=font_color, fontweight='bold')
+        
+    legend = plt.legend(marks, 
+                                bbox_to_anchor=(0.5, -0.4),
+                                prop = { "size": 16 },
+                                ncol=4, 
+                                fancybox=True)
+    for text in legend.get_texts():
+            plt.setp(text, color=font_color, alpha=0.7, fontweight='bold')
+            
+    fig.subplots_adjust(wspace=.18, hspace=.7) # Space between charts
+
+    title = fig.suptitle('Grades in dependence of groups', y=.95, fontweight='bold', fontsize=20, color=font_color)
+    # To prevent the title from being cropped
+    plt.subplots_adjust(top=0.85, bottom=0.26)
+    plt.savefig("groups" +".png")
+    plt.show()
+    
+    
+    
